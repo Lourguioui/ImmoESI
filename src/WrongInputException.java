985d1a099f4e028964313cebcba67dba879db264
Lourguioui <iz_lourguioui@esi.dz>
2020-01-09 08:17:33 +0100

    add the WrongInputExeption

---
+++ src/WrongInputException.java
@@ -0,0 +1,9 @@
+public class WrongInputException extends Exception {
+
+        private static final long serialVersionUID = 1L;
+        public WrongInputException() {
+            System.out.println("valeur entrée non valide!!!");
+        }
+
+
+}
